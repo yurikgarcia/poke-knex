d0b99ce2fd630ef3df451d84942fff36b9ecdeab
Barry <barrym07@gmail.com>
2021-10-29 14:52:38 -0600

    commit after making some changes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const knex = require('knex')(require('./knexfile')[process.env.NODE_ENV || 'deve
 app.use(express.json());
 
 
-app.get('/pokemon', function (req, res) {
+app.get('/api/pokemon', function (req, res) {
   knex
     .select('*')
     .from('pokemon')
@@ -31,6 +31,8 @@ async function getPokemon(poke) {
   let weight = result.weight;
   let picture = result.url;
   let base_exp = result.base_experience;
+  return name, poke_id, type, height, weight, picture, base_exp;
+
 
 
 };
@@ -50,32 +52,50 @@ app.get('/api/:pokemon', (req, res) => {
       } else {
         // fetch to pokeapi/${pokemon}
         //come back to this
-          //getPokemon(poke)
-        .fetch()
-          , then(res => {
-            let pokemon = res.data;
-            knex('pokemon').insert(({
-              name: pokemon.nam
-            }))
+        //getPokemon(poke)
+        getPokemon(pokemon)
+          .then(response => {
+            //come here first fuckers lol
+            //let pokemon = response.data;
+            knex('pokemon').insert({
+              Poke_Id: poke_id,
+              Name: name,
+              Type: type,
+              Base_Exp: base_exp,
+              Height: height,
+              Weight: weight,
+              Picture: picture
+            }).then(() => {
+              res.status(200).json(pokemon);
+            })
+              .catch(err => {
+                res.status(404)
+              })
           })
+          .end()
       }
     })
-
-
-  // knex and assign the data to the columns/attributes
-
-
 })
 
 
-app.get('/api/:pokemon/img', (req, res) => {
-  //if in db return image else fetch API for it
-  fetch(pokeAPI + req.params.id)
+// table.integer('Poke_Id');//result.id
+// table.string('Name').notNullable();//result.name
+// table.string('Type');//result.types.type
+// table.integer('Base_Exp');//result.base_experience
+// table.integer('Height');//result.height
+// table.integer('Weight');//result.weight
+// table.string('Picture');//result.url
+
+
+// knex and assign the data to the columns/attributes
+// app.get('/api/:pokemon/img', (req, res) => {
+//   //if in db return image else fetch API for it
+//   fetch(pokeAPI + req.params.id)
 
 
 
 
-})
+// })
 
 
 app.listen(PORT, () => {
--- migrations/20211029185333_create_pokemon.js
+++ migrations/20211029185333_create_pokemon.js
@@ -1,12 +1,12 @@
 exports.up = function (knex) {
   return knex.schema.createTable('pokemon', table => {
     table.increments('id'); // adds an auto incrementing PK column
-    table.int('Poke_Id');//result.id
+    table.integer('Poke_Id');//result.id
     table.string('Name').notNullable();//result.name
     table.string('Type');//result.types.type
-    table.int('Base Exp');//result.base_experience
-    table.int('Height');//result.height
-    table.int('Weight');//result.weight
+    table.integer('Base_Exp');//result.base_experience
+    table.integer('Height');//result.height
+    table.integer('Weight');//result.weight
     table.string('Picture');//result.url
   });
 };
