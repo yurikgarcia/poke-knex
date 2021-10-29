e5db18ed464f496aa0fce1828fd06ec9c9971b18
Barry <barrym07@gmail.com>
2021-10-29 13:54:01 -0600

    ideas sketched out

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ const knex = require('knex')(require('./knexfile')[process.env.NODE_ENV || 'deve
 
 app.use(express.json());
 
-app.get('/movies', function (req, res) {
+
+app.get('/pokemon', function (req, res) {
   knex
     .select('*')
-    .from('movies')
+    .from('pokemon')
     .then(data => res.status(200).json(data))
     .catch(err =>
       res.status(404).json({
@@ -19,31 +20,64 @@ app.get('/movies', function (req, res) {
     );
 });
 
+//result parse the json to pull out the content we want
+async function getPokemon(poke) {
+  let fetcher = await fetch(`https://pokeapi.co/api/v2/pokemon/${poke}`)
+  let result = await fetcher.json();
+  let name = result.name;
+  let poke_id = result.id;
+  let type = result.types.type;
+  let height = result.height;
+  let weight = result.weight;
+  let picture = result.url;
+  let base_exp = result.base_experience;
+
+
+};
+
+
+
+app.get('/api/:pokemon', (req, res) => {
+  let pokemon = req.params.pokemon;
+  //if in database return else fetch it
+  knex
+    .select('*')
+    .from('pokemon')
+    .where('name', pokemon)
+    .then(data => {
+      if (data.length > 0) {
+        res.status(200).json(data)
+      } else {
+        // fetch to pokeapi/${pokemon}
+        //come back to this
+          //getPokemon(poke)
+        .fetch()
+          , then(res => {
+            let pokemon = res.data;
+            knex('pokemon').insert(({
+              name: pokemon.nam
+            }))
+          })
+      }
+    })
+
+
+  // knex and assign the data to the columns/attributes
+
+
+})
+
+
+app.get('/api/:pokemon/img', (req, res) => {
+  //if in db return image else fetch API for it
+  fetch(pokeAPI + req.params.id)
+
+
+
+
+})
+
+
 app.listen(PORT, () => {
   console.log(`The server is running on ${PORT}`);
 });
-
-
-
-/*
-
-fetch("http://www.localhost:5000/",{
-        method: 'GET',
-        mode: "no-cors",
-        cache: "no-cache",
-        credentials: "same-origin",
-        headers: {"Content-Type": "application/json"}
-      })
-    .then(res=>{
-        console.log("res1",res)
-        return res.text();
-    })
-    .then(res=>{
-        console.log("res2",res)
-
-    })
-    .catch(res=>{
-        console.log("Exception : ",res);
-    })
-
-*/
--- migrations/20211029185333_create_pokemon.js
+++ migrations/20211029185333_create_pokemon.js
@@ -1,8 +1,16 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable('pokemon', table => {
+    table.increments('id'); // adds an auto incrementing PK column
+    table.int('Poke_Id');//result.id
+    table.string('Name').notNullable();//result.name
+    table.string('Type');//result.types.type
+    table.int('Base Exp');//result.base_experience
+    table.int('Height');//result.height
+    table.int('Weight');//result.weight
+    table.string('Picture');//result.url
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('pokemon');
 };
