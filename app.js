df35d212a7901939c216fe13bd12e29cd2fc69ce
Barry <barrym07@gmail.com>
2021-11-01 09:59:23 -0600

    all goals working

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const axios = require('axios');
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -6,10 +8,45 @@ const knex = require('knex')(require('./knexfile')[process.env.NODE_ENV || 'deve
 
 app.use(express.json());
 
+//Random pokemon
+app.get('/api/random', (req, res) => {
+  const randomId = Math.floor(Math.random() * 100) + 1;
+  console.log(randomId)
+  axios
+    .get(`https://pokeapi.co/api/v2/pokemon/${randomId}`)
+    .then(response => {
+      let pokemon = response.data;
+      knex('pokemon').insert({
+        Poke_Id: pokemon.id,
+        Name: pokemon.name,
+        Type: pokemon.types[0].type.name,
+        Base_Exp: pokemon.base_experience,
+        Height: pokemon.height,
+        Weight: pokemon.weight,
+        Picture: pokemon.sprites.front_default
+      }).then(() => {
+        res.status(200).send({
+          Poke_Id: pokemon.id,
+          Name: pokemon.name,
+          Type: pokemon.types[0].type.name,
+          Base_Exp: pokemon.base_experience,
+          Height: pokemon.height,
+          Weight: pokemon.weight,
+          Picture: pokemon.sprites.front_default
+        })
+      })
+    })
+    .catch(err => {
+      res.status(404).json({
+        message: `The pokemon with ID of ${randomId} could not be found.`
+      })
+    })
+})
 
+//All Pokemon in the DB's name
 app.get('/api/pokemon', function (req, res) {
   knex
-    .select('*')
+    .select('Name')
     .from('pokemon')
     .then(data => res.status(200).json(data))
     .catch(err =>
@@ -20,82 +57,116 @@ app.get('/api/pokemon', function (req, res) {
     );
 });
 
-//result parse the json to pull out the content we want
-async function getPokemon(poke) {
-  let fetcher = await fetch(`https://pokeapi.co/api/v2/pokemon/${poke}`)
-  let result = await fetcher.json();
-  let name = result.name;
-  let poke_id = result.id;
-  let type = result.types.type;
-  let height = result.height;
-  let weight = result.weight;
-  let picture = result.url;
-  let base_exp = result.base_experience;
-  return name, poke_id, type, height, weight, picture, base_exp;
+//All Pokemon sorted by Base_Exp
+app.get('/api/pokemon/exp', function (req, res) {
+
+  knex
+    .select('*')
+    .from('pokemon')
+    .orderBy('Base_Exp', 'desc')
+    .then(data => res.status(200).json(data))
+    .catch(err =>
+      res.status(404).json({
+        message:
+          'The data you are looking for could not be found. Please try again'
+      })
+    );
+});
 
 
-
-};
-
-
-
+//if in the DB show them else fetch
 app.get('/api/:pokemon', (req, res) => {
   let pokemon = req.params.pokemon;
   //if in database return else fetch it
   knex
     .select('*')
     .from('pokemon')
-    .where('name', pokemon)
+    .where('Name', pokemon)
     .then(data => {
       if (data.length > 0) {
         res.status(200).json(data)
       } else {
-        // fetch to pokeapi/${pokemon}
-        //come back to this
-        //getPokemon(poke)
-        getPokemon(pokemon)
+        axios
+          .get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
           .then(response => {
-            //come here first fuckers lol
-            //let pokemon = response.data;
+            let pokemon = response.data;
             knex('pokemon').insert({
-              Poke_Id: poke_id,
-              Name: name,
-              Type: type,
-              Base_Exp: base_exp,
-              Height: height,
-              Weight: weight,
-              Picture: picture
+              Poke_Id: pokemon.id,
+              Name: pokemon.name,
+              Type: pokemon.types[0].type.name,
+              Base_Exp: pokemon.base_experience,
+              Height: pokemon.height,
+              Weight: pokemon.weight,
+              Picture: pokemon.sprites.front_default
             }).then(() => {
-              res.status(200).json(pokemon);
+              res.status(200).send({
+                Poke_Id: pokemon.id,
+                Name: pokemon.name,
+                Type: pokemon.types[0].type.name,
+                Base_Exp: pokemon.base_experience,
+                Height: pokemon.height,
+                Weight: pokemon.weight,
+                Picture: pokemon.sprites.front_default
+              });
             })
-              .catch(err => {
-                res.status(404)
-              })
           })
-          .end()
+          .catch(err => {
+            res.status(404).json({
+              message: `The pokemon you are looking for could not be found. Please try again`
+            })
+          })
       }
     })
 })
 
 
-// table.integer('Poke_Id');//result.id
-// table.string('Name').notNullable();//result.name
-// table.string('Type');//result.types.type
-// table.integer('Base_Exp');//result.base_experience
-// table.integer('Height');//result.height
-// table.integer('Weight');//result.weight
-// table.string('Picture');//result.url
+//checks for image, if not in DB fetches API and stores
+app.get('/api/:pokemon/img', (req, res) => {
+  let pokemon = req.params.pokemon;
+  //if in database return else fetch it
+  knex
+    .select('Picture')
+    .from('pokemon')
+    .where('Name', pokemon)
+    .then(data => {
+      if (data.length > 0) {
+        res.status(200).json(data)
+      } else {
+        axios
+          .get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+          .then(response => {
+            let pokemon = response.data;
+            knex('pokemon').insert({
+              Poke_Id: pokemon.id,
+              Name: pokemon.name,
+              Type: pokemon.types[0].type.name,
+              Base_Exp: pokemon.base_experience,
+              Height: pokemon.height,
+              Weight: pokemon.weight,
+              Picture: pokemon.sprites.front_default
+            }).then(() => {
+              res.status(200).send({
+                Poke_Id: pokemon.id,
+                Name: pokemon.name,
+                Type: pokemon.types[0].type.name,
+                Base_Exp: pokemon.base_experience,
+                Height: pokemon.height,
+                Weight: pokemon.weight,
+                Picture: pokemon.sprites.front_default
+              });
+            })
+          })
+          .catch(err => {
+            res.status(404).json({
+              message: `The pokemon you are looking for could not be found. Please try again`
+            })
+          })
+      }
+    })
+})
 
 
-// knex and assign the data to the columns/attributes
-// app.get('/api/:pokemon/img', (req, res) => {
-//   //if in db return image else fetch API for it
-//   fetch(pokeAPI + req.params.id)
 
-
-
-
-// })
 
 
 app.listen(PORT, () => {
